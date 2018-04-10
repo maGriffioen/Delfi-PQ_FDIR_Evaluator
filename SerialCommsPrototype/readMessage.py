555b2f0a58fa9fe2e63818a6b9e09a9acb9bcf9f
Viktor Jordanov <viko27@gmail.com>
2018-04-10 13:58:36 +0200

    Added Windows compatibility

--- SerialCommsPrototype/readMessage.py
+++ SerialCommsPrototype/readMessage.py
@@ -4,9 +4,9 @@ import serial		#-Install this module first-
 import time
 
 
-ser = serial.Serial('/dev/ttyACM0', 9600)	#TI-MSP432 - Linus
+#ser = serial.Serial('/dev/ttyACM0', 9600)	#TI-MSP432 - Linus
 #ser = serial.Serial('/dev/ttyUSB0', 9600)	#Arduino clone - Linux
-	
+ser = serial.Serial('COM3', 9600)	#TI-MSP432 - Windows
 
 while True:
 	time.sleep(0.1)
