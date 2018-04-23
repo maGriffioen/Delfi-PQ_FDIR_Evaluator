329e4c7564cd47bc7393dc0626e874b818daeabf
FlyOHolic <m.a.griffioen@student.tudelft.nl>
2018-04-23 13:25:51 +0200

    Pie chart

--- pythonHost/pythonHost.py
+++ pythonHost/pythonHost.py
@@ -1,6 +1,7 @@
 from controllerInterface import controllerInterface
 import time
 import numpy
+from matplotlib import pyplot
 
 controllerPort = '/dev/ttyACM1'
 resetPort = '/dev/ttyACM2'
@@ -9,10 +10,10 @@ ctr = controllerInterface(controllerPort, resetPort)
 markerbegin = 536870912
 
 searchspace = 1048576
-searchspace = 60000
-looprange = 4
+searchspace = 64000
+looprange = 20
 
-locnumber = 25
+locnumber = 5
 numpy.random.seed(5)
 floatarray = numpy.around(markerbegin + searchspace * numpy.random.rand(1, locnumber))
 randloc = floatarray.astype(int).tolist()[0]
@@ -26,8 +27,9 @@ for i in randloc:
         addition = i + j
         fulllist.append(addition)
 
-flipLocations = [3, 18, 29]
+flipLocations = [3]
 
+starttime = time.time()
 
 resultList = []
 print "Start FDIR verification"
@@ -39,7 +41,7 @@ for markerloc in fulllist:
         o1 = ctr.output()
         f1 = ctr.flip( flipLocation )
         o2 = ctr.output()
-        time.sleep(0.2)
+        time.sleep(0.1)
 
         dataVerified = ctr.verifyData()
 
@@ -67,8 +69,19 @@ for markerloc in fulllist:
         #print ctr.serialLog
 #print resultList
 
+print "FDIR-Verification done. Runtime: " + str(time.time() - starttime) + " s"
+print resultList
+numberOfFailureTypes = 5
 thefile = open('output.txt', 'w')
-
+failureCount = [0 for i in range(numberOfFailureTypes)]
 for result in resultList:
     thefile.write( str(result[0]) + ', ' + str(result[1]) + ', ' + str(result[2]) + '\n' )
+    failureCount[ result[2] ] += 1
+
 thefile.close()
+
+legend = ['None', 'Lockup', 'Data corruption', 'Move failure', 'Move fault']
+legend = [legend[i] + " " + str( failureCount[i]) for i in range(numberOfFailureTypes)]
+pyplot.pie(failureCount, labels = legend)
+pyplot.title("Failure distributions")
+pyplot.show()
